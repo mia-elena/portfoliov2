2f940b2fe386d0368e3512dc61d81c58f3511d54
Mia E. <miaskyelena@gmail.com>
2025-10-11 12:52:54 -0400

    update resume link

--- my-portfolio/src/config/profile.ts
+++ my-portfolio/src/config/profile.ts
@@ -9,7 +9,7 @@ export const profile = {
     github: "https://github.com/miasdk",
     linkedIn: "https://www.linkedin.com/in/miaelena/",
     website: "https://miaelena.vercel.app/",
-    resume: "https://www.dropbox.com/scl/fi/cizrd3ho6tvx3jtfhxr1c/Mia-Elena-Ricci-SWE-Resume.pdf?rlkey=2riqxf44fe879gldnjj4m2d09&st=z6zu0tv3&dl=0",
+    resume: "https://www.dropbox.com/scl/fi/apvvtjqsnv2via15zvufq/Mia-Elena-Tapia-SWE-Resume.pdf?rlkey=63sdhal4calzoax41jmsinc0e&st=lrtjj90s&dl=0",
   },
   skills: [
     "python",
