6fa205e88f42e2a89cb5f11ab78ae9a91e011dc7
Mia E. <miaskyelena@gmail.com>
2025-10-12 17:10:52 -0400

    update profile section

--- my-portfolio/src/config/profile.ts
+++ my-portfolio/src/config/profile.ts
@@ -9,7 +9,7 @@ export const profile = {
     github: "https://github.com/miasdk",
     linkedIn: "https://www.linkedin.com/in/miaelena/",
     website: "https://miaelena.vercel.app/",
-    resume: "https://www.dropbox.com/preview/Mia%20Elena%20Ricci%20SWE%20Resume.pdf?context=content_suggestions&role=personal",
+    resume: "https://www.dropbox.com/scl/fi/apvvtjqsnv2via15zvufq/Mia-Elena-Tapia-SWE-Resume.pdf?rlkey=63sdhal4calzoax41jmsinc0e&st=i2rdfn5o&dl=0",
   },
   skills: [
     "python",
